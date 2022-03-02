521974d1781db2de0b3ce0419a493d076dd900cb
Justinian <justin.erdmier@outlook.com>
2022-03-01 20:31:53 -0600

    Updating styling for project one.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,18 @@
  * Licensed under the MIT License - See LICENSE in repository root.                               *
  **************************************************************************************************/
 
-import React from 'react';
+import React     from 'react';
+import reactIcon from '../images/react-icon.png';
 import '../index.css';
 
 export default function Navbar() {
     return (
         <>
-            <h1>Navbar goes here.</h1>
+            <nav>
+                <img src={reactIcon} alt='React Logo' className='nav--icon' />
+                <h3 className='nav--logo_text'>React Facts</h3>
+                <h4 className='nav--title'>React Course - Project 1</h4>
+            </nav>
         </>
     );
 }
